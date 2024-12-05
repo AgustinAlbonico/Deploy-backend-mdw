34d22e6b80d124875b46e8056423f02a36bfaa5f
AgustinAlbonico <agustin_010@live.com>
2024-12-05 17:20:45 -0300

    asdqwe

--- src/config/connectDb.ts
+++ src/config/connectDb.ts
@@ -3,6 +3,7 @@ import { MONGODB_URI } from "./env";
 
 const connectDb = async () => {
   try {
+    console.log(MONGODB_URI)
     await mongoose.connect(MONGODB_URI);
     console.log("Conexion a la DB exitosa");
   } catch (error) {
